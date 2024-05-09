5d14479cad6b25b8e99a92a0e6015489a928c343
MHammad4968 <hammad.mh.4968@gmail.com>
2024-05-09 18:01:25 +0530

    Boilerplate + basic frontend + node

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # skillindia-national-practice
 Practicing on previous years' tasks.
+
+
+Note: Am including NodeModules for convenience since this is to be deployed on Lambda, and modules are required to be in the project directory there.
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// Dependancies
 const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const serverless = require("serverless-http");
+const util = require("./utils.js");
+
+//App from here
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Order function
+app.post("/order", (req, res) => {
+    const order = req.body.order;
+    console.log(JSON.stringify(order));
+    res.send(`Order received for ${order}`);
+    console.log(util.getOrderID());
+});
+
+app.listen(3069);
+console.log("Listening on port 3069");
